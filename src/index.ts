9fe3b3fc5971aed1e6310388dec48393fe4c8906
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-07-31 20:30:23 -0300

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
-import { pickCookie } from './pickCookie.js';
+import cors from 'cors'
+import { pickCookie } from './pickCookie.js'
 
-
-const app = express();
 const port = process.env.PORT ?? 4000;
 
+const app = express();
+
+app.use(cors());
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
